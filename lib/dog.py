6df3ab58c555a49a4ce06fa6de9dd1f4065a59e1
patrickonesiah <patrickonesiah@gmail.com>
2024-02-08 20:11:18 +1100

    lab completed

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,13 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
     pass
+
+fido = Dog()
+fido.sit()
+
